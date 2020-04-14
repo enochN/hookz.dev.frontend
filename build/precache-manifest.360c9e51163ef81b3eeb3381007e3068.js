bd6b372343bfa111c5d42ba4d8bcb9de460a99ed
Rasheeda Yehuza <ryehuza@gmail.com>
2020-04-14 22:39:09 +0200

    new build - unable to build on lightsail

---
+++ build/precache-manifest.360c9e51163ef81b3eeb3381007e3068.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c5c802d9e280aeb8b048feb8023eaae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9aea3267cafa90180238",
+    "url": "/static/css/main.e9f357de.chunk.css"
+  },
+  {
+    "revision": "abfd82a0fc7092d1d685",
+    "url": "/static/js/2.dce8d2ba.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.dce8d2ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aea3267cafa90180238",
+    "url": "/static/js/main.c3f1fb68.chunk.js"
+  },
+  {
+    "revision": "8bf69297da9cf43fc69a",
+    "url": "/static/js/runtime-main.da9796fe.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0c367ca0c530de52d5493db692c906a9.js"
+  "/precache-manifest.360c9e51163ef81b3eeb3381007e3068.js"
 );
 
 self.addEventListener('message', (event) => {
